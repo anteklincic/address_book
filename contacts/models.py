50c10ea022e9c6093090eaf64753de7f4fb8ec98
Ante Klincic <ante.klincic@gmail.com>
2024-03-30 15:31:29 +0100

    CRUD working

--- contacts/models.py
+++ contacts/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField()
--- contacts/views.py
+++ contacts/views.py
@@ -1,29 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from .models import Contact
 from .forms import ContactForm
 
-@login_required
 def contact_list(request):
-    contacts = Contact.objects.filter(user=request.user)
+    contacts = Contact.objects.all()
     return render(request, 'contacts/contact_list.html', {'contacts': contacts})
 
-@login_required
 def contact_create(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            contact = form.save(commit=False)
-            contact.user = request.user
-            contact.save()
+            form.save()
             return redirect('contact_list')
     else:
         form = ContactForm()
     return render(request, 'contacts/contact_form.html', {'form': form})
 
-@login_required
 def contact_update(request, pk):
-    contact = get_object_or_404(Contact, pk=pk, user=request.user)
+    contact = get_object_or_404(Contact, pk=pk)
     if request.method == 'POST':
         form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
@@ -33,9 +27,8 @@ def contact_update(request, pk):
         form = ContactForm(instance=contact)
     return render(request, 'contacts/contact_form.html', {'form': form})
 
-@login_required
 def contact_delete(request, pk):
-    contact = get_object_or_404(Contact, pk=pk, user=request.user)
+    contact = get_object_or_404(Contact, pk=pk)
     if request.method == 'POST':
         contact.delete()
         return redirect('contact_list')
